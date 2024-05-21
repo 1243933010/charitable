2b1b4cdcb3efc4cccf7a1f40ee2471cf24b8a57e
yyy <1243933010@qq.com>
2024-05-21 23:43:32 +0800

    feat(): 添加义卖产品

--- pages/index/addProduct.vue
+++ pages/index/addProduct.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="profix-page-container">
 		<hx-navbar :config="config" style="position: absolute; top: 0; width: 100vw;" />
-		<view class="add-products-scroll page-scroll">
+		<scroll-view :scroll-y="true" class="add-products-scroll page-scroll">
 			<view class="banner">
 				<swiper class="swiper" circular style="height: 430rpx;" @change="swiperChange">
 					<swiper-item v-for="(item, index) in swiperList" :key="index">
@@ -28,15 +28,60 @@
 				</view>
 				
 				<div class="input-box">
-					<div class="label">商品名称</div>
-					<input type="text" class="inp" placeholder="请输入商品名称..." />
+					<div class="label">{{$t("app.shen9")}}</div>
+					<input type="text" class="inp" :placeholder="$t('app.shen10')" />
+				</div>
+				
+				<div class="input-box">
+					<div class="label">{{$t("app.shen11")}}</div>
+					<input type="text" class="inp" :placeholder="$t('app.shen12')" />
+				</div>
+				
+				<div class="input-box">
+					<div class="label">{{$t("app.shen13")}}</div>
+					<input type="text" class="inp" :placeholder="$t('app.shen14')" />
+				</div>
+			</view>
+			
+			<view class="gray-bg">
+				<div class="input-box">
+					<div class="label">{{$t("productDetail.pageTit")}}</div>
+					<div class="upload-pic-list">
+						<div class="upload-item" v-for="(item,index) in swiperList" :key="index">
+							<div class="box">
+								<div class="pic">
+									<image :src="item.image" class="img" mode="widthFix"></image>
+								</div>
+								<div class="del-btn" @click="delHandle(item)">
+									<div class="pic">
+										<image src="@/static/img/icon/del.png" class="img" mode="widthFix"></image>
+									</div>
+									<p>删除图片</p>
+								</div>
+							</div>
+						</div>
+						<div class="upload-item">
+							<div class="box">
+								<div class="up-btn">
+									<div class="pic">
+										<image src="@/static/img/icon/upload.png" class="img" mode="widthFix"></image>
+									</div>
+									<p>删除图片</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="input-box">
+					<div class="label">{{$t("productDetail.pageTit")}}</div>
+					<textarea class="inp-textarea" :placeholder="$t('app.shen15')"></textarea>
 				</div>
 			</view>
 			
 			<div class="add-product">
 				<button class="button" @click="submitHandle">{{$t("app.shen7")}}</button>
 			</div>
-		</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -73,7 +118,6 @@
 			};
 		},
 		mounted() {
-			this.handleIsShow();
 		},
 		methods: {
 			goPage(url) {
@@ -88,6 +132,9 @@
 			      // e.detail.current 是当前的索引
 			      this.currentIndex = e.detail.current;
 			},
+			delHandle(uploadItem) {
+				
+			}
 		}
 	}
 </script>
@@ -102,6 +149,102 @@ page {
 .profix-page-container {
 	.add-products-scroll {
 		padding: 88rpx 0 0;
+		
+		.input-box {
+			margin-top: 32rpx;
+			
+			.label {
+				margin-bottom: 20rpx;
+				padding-left: 25rpx;
+				color: #3A2633;
+				font-size: 30rpx;
+				line-height: 1.4;
+				font-weight: bold;
+			}
+			
+			.inp {
+				border-radius: 10rpx;
+				padding: 30rpx 25rpx;
+				background-color: #F4F4F4;
+				color: #BCA988;
+				display: block;
+				height: auto;
+				line-height: 1;
+			}
+		
+			.upload-pic-list {
+				.df();
+				flex-wrap: wrap;
+				
+				.upload-item {
+					padding: 0 9rpx;
+					width: 33.33%;
+					
+					.box {
+						border-radius: 20rpx;
+						overflow: hidden;
+						position: relative;
+						height: 100%;
+						
+						.pic {
+							width: 100%;
+						}
+						
+						.del-btn {
+							.df(center, center);
+							
+							position: absolute;
+							bottom: 0;
+							left: 0;
+							right: 0;
+							padding: 10rpx;
+							background-color: rgba(0, 0, 0, .53);
+							
+							.pic {
+								margin-right: 10px;
+								width: 24rpx;
+								height: 24rpx;
+							}
+							
+							p {
+								font-size: 20rpx;
+								color: #fff;
+							}
+						}
+						
+						.up-btn {
+							.df(center, center);
+							flex-direction: column;
+							background-color: #fff;
+							height: 100%;
+							
+							.pic {
+								width: 44rpx;
+								height: 44rpx;
+							}
+							
+							p {
+								margin-top: 12rpx;
+								font-size: 24rpx;
+								color: #BCA988;
+								line-height: 1.375;
+							}
+						}
+					}
+				}
+			}
+			
+			.inp-textarea {
+				border-radius: 10rpx;
+				border: 1rpx solid #707070;
+				padding: 32rpx 25rpx;
+				width: 100%;
+				color: #BCA988;
+				font-size: 26rpx;
+				height: 329rpx;
+				background-color: transparent;
+			}
+		}
 		
 		.banner {
 			width: 100%;
@@ -155,29 +298,10 @@ page {
 					border-radius: 50rpx;
 				}
 			}
-			
-			.input-box {
-				margin-top: 32rpx;
-				
-				.label {
-					margin-bottom: 20rpx;
-					padding-left: 25rpx;
-					color: #3A2633;
-					font-size: 30rpx;
-					line-height: 1.4;
-					font-weight: bold;
-				}
-				
-				.inp {
-					border-radius: 10rpx;
-					padding: 30rpx 25rpx;
-					background-color: #F4F4F4;
-					color: #BCA988;
-					display: block;
-					height: auto;
-					line-height: 1;
-				}
-			}
+		}
+		
+		.gray-bg {
+			padding: 20rpx 30rpx 200rpx;
 		}
 		
 		.add-product {
